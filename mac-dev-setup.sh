f1e925bad8555a94715d843ba1564831c000699a
Sam Mingo <github@lamolabs.org>
2021-09-19 16:40:45 -0400

    Adds keyboard times

--- mac-dev-setup.sh
+++ mac-dev-setup.sh
@@ -53,6 +53,11 @@ esac
 # Temporarily disable gatekeeper to prevent installations from failing
 sudo spctl --master-disable
 
+# Speed up keypresses
+# https://apple.stackexchange.com/questions/10467/how-to-increase-keyboard-key-repeat-rate-on-os-x
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
 # Install homebrew if it's not already installed
 type brew &> /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
